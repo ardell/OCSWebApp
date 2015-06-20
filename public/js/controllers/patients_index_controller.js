d143a1e9cdf52839bc0a7f7e11739ceba58556c2
Jason Ardell <ardell@gmail.com>
2015-06-20 13:45:30 -0400

    Allow user to sort by any field.

--- public/js/controllers/patients_index_controller.js
+++ public/js/controllers/patients_index_controller.js
@@ -1,19 +1,24 @@
 angular
   .module('ocs')
   .controller('PatientsIndexController', function($scope, Patient) {
+    window.s = $scope;
+
     var PAGE_SIZE        = 100000;
     $scope.filterByStudy = 'Any study';
     $scope.search        = '';
-    $scope.sortBy        = 'examDate';
-    $scope.sortDirection = 'descending';
+    $scope.sortColumn    = 'examDate';
+    $scope.sortAscending = false;
     $scope.patients      = [];
     $scope.studies       = [];
     $scope.loading       = false;
 
     var fetchPatients = function() {
+      var direction = 'descending';
+      if ($scope.sortAscending) direction = 'ascending';
+
       var query = new Parse.Query(Patient);
       query.limit(PAGE_SIZE);
-      query[$scope.sortDirection]($scope.sortBy);
+      query[direction]($scope.sortColumn);
       if ($scope.filterByStudy != 'Any study') {
         query.equalTo("study", $scope.filterByStudy);
       }
@@ -21,7 +26,22 @@ angular
       query.find({
         success: function(arr) {
           $scope.$apply(function() {
-            $scope.patients = arr;
+            $scope.patients = _.map(arr, function(obj) {
+              var attrs = obj.attributes;
+              delete attrs.EyeImages;
+              attrs.fullName = [attrs.lastName, attrs.firstName].join(', ');
+
+              if (attrs.examDate) {
+                attrs.formattedExamDate = [
+                  attrs.examDate.toLocaleDateString(),
+                  attrs.examDate.toLocaleTimeString()
+                ].join(' ');
+              } else {
+                attrs.formattedExamDate = null;
+              }
+
+              return attrs;
+            });
             $scope.loading  = false;
 
             // Only set up the list of studies the first time (while the list
@@ -47,16 +67,16 @@ angular
     fetchPatients($scope.filterByStudy, $scope.sort);
     $scope.$watch('filterByStudy', fetchPatients);
 
-    // Coordinate changes from sortBy to sortDirection
-    $scope.$watch('sortBy', function() {
-      if (!$scope.sortBy) return;
-      if ($scope.sortBy == 'examDate') {
-        $scope.sortDirection = 'descending';
+    // Handle requests to change sort order
+    $scope.sort = function(column) {
+      if ($scope.sortColumn == column) {
+        // If we're already sorted by that column, toggle the order
+        $scope.sortAscending = !$scope.sortAscending;
       } else {
-        $scope.sortDirection = 'ascending';
+        // Otherwise, sort by the column and reset direction
+        $scope.sortColumn = column;
+        $scope.sortAscending = ($scope.sortColumn != 'examDate');
       }
-
-      fetchPatients();
-    });
+    };
   });
 
